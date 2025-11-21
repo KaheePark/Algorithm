1802ddb6082b1fcf9a34c88ec5428bd56a8f4748
KaheePark <mydnjang@naver.com>
2025-11-21 22:39:38 +0900

    정렬

--- Programmers/level2/가장큰수.java
+++ Programmers/level2/가장큰수.java
@@ -11,15 +11,16 @@ public class 가장큰수 {
             number_tmp[i] = String.valueOf(numbers[i]);
         }
 
-        // 문자열 붙여서 비교 후 내림차순 정렬
+        // 문자열끼리 이어붙여서 크기 비교 후 내림차순 정렬
         Arrays.sort(number_tmp, (a, b) -> (b+a).compareTo(a+b));
 
-        // 붙힌 후 숫자로 변환
+        // 배열 모두 붙이기
         for(int i=0; i<number_tmp.length; i++){
             sb.append(number_tmp[i]);
         }
         answer = sb.toString();
 
+        // 배열이 모두 0인경우 "0" 반환
         if(answer.startsWith("0")){
             answer = "0";
         }
